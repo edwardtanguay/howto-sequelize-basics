65e73f65830907f77a1178bb97c206c1635893cc
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-29 01:12:54 +0200

    validate

--- models/notes.js
+++ models/notes.js
@@ -1,24 +1,40 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Notes extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Notes.init({
-    title: DataTypes.STRING,
-    body: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Notes',
-  });
-  return Notes;
+	class Notes extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+
+		toJSON() {
+			return {...this.get(), id: undefined};
+		}
+	}
+	Notes.init(
+		{
+			uuid: {
+				type: DataTypes.UUID,
+				defaultValue: DataTypes.UUIDV4
+			},
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				validate: {
+					notNull: { msg: 'Title may not be null.' },
+					notEmpty: {msg: 'Title may not be empty.'}
+				}
+			},
+			body: DataTypes.STRING
+		},
+		{
+			sequelize,
+			modelName: 'Notes'
+		}
+	);
+	return Notes;
 };
--- server.js
+++ server.js
@@ -1,7 +1,51 @@
-const { sequelize } = require('./models');
+const { sequelize, Notes } = require('./models');
+const express = require('express');
 
-async function main() {
-	await sequelize.sync();
-}
+const app = express();
+app.use(express.json());
+const port = 3333;
 
-main();
+app.get('/', (req, res) => {
+    res.send('<h1>ORM</h1>');
+});
+
+app.post('/notes', async (req, res) => {
+    const { title, body } = req.body;
+
+    try {
+        const note = await Notes.create({ title, body });
+        return res.json(note);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json(err);
+    }
+});
+
+app.get('/notes', async (req, res) => {
+    try {
+        const notes = await Notes.findAll();
+        return res.json(notes);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'There was an error.' });
+    }
+});
+
+app.get('/notes/:uuid', async (req, res) => {
+    const uuid = req.params.uuid;
+    try {
+        const note= await Notes.findOne({
+            where: { uuid }
+        });
+        return res.json(note);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'There was an error.' });
+    }
+});
+
+app.listen(port, async () => {
+    console.log(`Server running on http://localhost:${port}`);
+    await sequelize.sync();
+    console.log('Database synced.');
+});
